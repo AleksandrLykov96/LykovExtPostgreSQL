fb5c4919f58031b1c139cc64b52b81fdc0b8c53a
Aleksandr Lykov <aleksandr@lykov96.ru>
2023-07-25 09:55:35 +0300

    Добавьте файлы проекта.

---
+++ LykovExtPostgreSQL.cpp
@@ -0,0 +1,12 @@
+﻿// LykovExtPostgreSQL.cpp: определяет точку входа для приложения.
+//
+
+#include "LykovExtPostgreSQL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
